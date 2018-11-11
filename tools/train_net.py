e3791e4dcc18acceae3119d780b09bb36370872f
JiazeWang <jiazewang@csu.edu.cn>
2018-11-11 19:03:43 +0800

    fix

--- tools/train_net.py
+++ tools/train_net.py
@@ -50,7 +50,7 @@ def train(cfg, local_rank, distributed):
     checkpointer = DetectronCheckpointer(
         cfg, model, optimizer, scheduler, output_dir, save_to_disk
     )
-    print(cfg.MODEL.WEIGHT)
+    print('****************',cfg.MODEL.WEIGHT,'***************')
     extra_checkpoint_data = checkpointer.load(cfg.MODEL.WEIGHT)
     arguments.update(extra_checkpoint_data)
 
